00b61dda9d8339178812783ecc0359f85bf0afea
NaskoVasilev <nasko_vasilev@abv.bg>
2019-05-21 00:47:05 +0300

    Merge sort and Parallel Merge sort implementations

---
+++ Asynchronous Processing/ParallelMergeSort/ParallelMergeSort/MergeSort.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace ParallelMergeSort
+{
+	public static class MergeSort<T> where T : IComparable
+	{
+		public static void Sort(T[] arr)
+		{
+			Sort(arr, 0, arr.Length - 1);
+		}
+
+		private static void Sort(T[] arr, int startIndex, int endIndex)
+		{
+			if (startIndex >= endIndex)
+			{
+				return;
+			}
+
+			//Get index of the middle element
+			int middleIndex = (startIndex + endIndex) / 2;
+
+			//Sort left portion of the array
+			Sort(arr, startIndex, middleIndex);
+
+			//Sort right portion of the array
+			Sort(arr, middleIndex + 1, endIndex);
+
+			//Merge left and right portion of the array
+			Merge(arr, startIndex, middleIndex, endIndex);
+		}
+
+		private static void Merge(T[] arr, int startIndex, int middleIndex, int endIndex)
+		{
+			if (middleIndex < 0 || middleIndex + 1 >= arr.Length || arr[middleIndex].CompareTo(arr[middleIndex + 1]) <= 0)
+			{
+				return;
+			}
+
+			T[] helperArray = new T[arr.Length];
+
+			for (int i = startIndex; i <= endIndex; i++)
+			{
+				helperArray[i] = arr[i];
+			}
+
+			//Start index of the first portion of the array
+			int leftIndex = startIndex;
+			//Start index of the second portion of the array
+			int rightIndex = middleIndex + 1;
+
+			for (int i = startIndex; i <= endIndex; i++)
+			{
+				if (leftIndex > middleIndex)
+				{
+					arr[i] = helperArray[rightIndex++];
+				}
+				else if (rightIndex > endIndex)
+				{
+					arr[i] = helperArray[leftIndex++];
+				}
+				else if (helperArray[leftIndex].CompareTo(helperArray[rightIndex]) <= 0)
+				{
+					arr[i] = helperArray[leftIndex++];
+				}
+				else
+				{
+					arr[i] = helperArray[rightIndex++];
+				}
+			}
+		}
+	}
+}
---
+++ Asynchronous Processing/ParallelMergeSort/ParallelMergeSort/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace ParallelMergeSort
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Statistics for int[] with 240 000 elementss
+			// MergeSort<int>.Sort(arr); -> 00:00:24.9471281
+			// new MergeSortHelper<int>().Sort(arr); -> 00:00:00.2677476
+			// new MergeSortHelper<int>().SortParallel(arr); -> 00:00:00.1520289
+
+			string numbers = File.ReadAllText("../../../numbers.txt");
+			int[] arr = numbers.Split(", ")
+				.Select(int.Parse)
+				.ToArray();
+
+			Console.WriteLine("Number of elements: " + arr.Length);
+
+			Stopwatch stopwatch = new Stopwatch();
+			stopwatch.Start();
+
+			//MergeSort<int>.Sort(arr);
+			//new MergeSortHelper<int>().Sort(arr);
+			new MergeSortHelper<int>().SortParallel(arr);
+			Console.WriteLine(stopwatch.Elapsed);
+
+			bool isSort = true;
+
+			for (int i = 0; i < arr.Length - 1; i++)
+			{
+				if(arr[i] > arr[i + 1])
+				{
+					isSort = false;
+				}
+			}
+
+			Console.WriteLine(isSort);
+		}
+	}
+}
+
