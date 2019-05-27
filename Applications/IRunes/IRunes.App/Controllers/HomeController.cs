3d815a05873d5097ec2632df67acf6463c19d635
NaskoVasilev <nasko_vasilev@abv.bg>
2019-05-27 19:38:58 +0300

    Develop IRunes app using custom web server and bootstrap for forntend part

---
+++ Applications/IRunes/IRunes.App/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using SIS.HTTP.Requests.Contracts;
+using SIS.HTTP.Responses.Contracts;
+
+namespace IRunes.App.Controllers
+{
+	public class HomeController : BaseController
+	{
+		public IHttpResponse Index(IHttpRequest httpRequest)
+		{
+			if (this.IsLogedIn(httpRequest))
+			{
+				ViewData["username"] = GetUsername(httpRequest);
+				return View("Index-Logged");
+			}
+
+			return View();
+		}
+	}
+}
---
+++ Applications/IRunes/SIS.WebServer/Result/InlineResourceResult.cs
@@ -0,0 +1,16 @@
+﻿namespace SIS.WebServer.Result
+{
+	using SIS.HTTP.Enums;
+	using SIS.HTTP.Headers;
+	using SIS.HTTP.Responses;
+
+	public class InlineResourceResult : HttpResponse
+	{
+		public InlineResourceResult(byte[] content, HttpResponseStatusCode statusCode) : base(statusCode)
+		{
+			this.Headers.AddHeader(new HttpHeader(HttpHeader.ContentLength, content.Length.ToString()));
+			this.Headers.AddHeader(new HttpHeader(HttpHeader.ContentDisposition, "inline"));
+			this.Content = content;
+		}
+	}
+}
